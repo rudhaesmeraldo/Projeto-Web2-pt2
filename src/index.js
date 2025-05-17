94a85220532b3e039802bf8a3bf06c67f58e1107
Niccofs <nifesilva@outlook.com>
2025-05-17 14:09:36 -0300

    docs: addind .vercel to gitignore

--- src/index.js
+++ src/index.js
@@ -149,4 +149,4 @@ app.get('/laboratorio/relatorio', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+//app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
